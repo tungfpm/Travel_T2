09b43d3f477994cbe31b14a181cb490ca65fed6d
Phạm Mạnh Tùng <tungfpm@gmail.com>
2015-11-24 00:05:58 +0700

    Begin1_KhungTrang

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Travel</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+	<link rel="stylesheet" type="text/css" href="css/justified-nav.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body class="green">
+<div class="container yellow">
+	<div class="col-xs-12 header">
+		<div class="col-xs-3 vertical">
+			<a class="navbar-brand no-padding" href="#">
+	        	<img alt="Brand" src="images/ta.jpg" height="100%">
+	     	</a>
+	     	<p class= "vertical-middle"> Welcome to Tungfpm </p>
+		</div>
+		<div class="col-xs-5 vertical">
+			<h3>Website giới thiệu du lịch</h3>
+		</div>
+		<div class="col-xs-4">
+			<ul class="nav nav-pills">
+			  <li role="presentation" class="active"><a href="index.php">Trang chủ</a></li>
+			  <li role="presentation"><a href="#">Đăng nhập</a></li>
+			  <li role="presentation"><a href="#">Đăng ký</a></li>
+			</ul>
+		</div>
+	</div>
+	<br>
+	<div class="col-xs-12 menu-nav">
+        <nav>
+          <ul class="nav nav-justified">
+            <li class="active"><a href="#">Trang chủ</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+
+	<div class="col-xs-12 search" >
+		<nav class="navbar navbar-default">
+		  	<div class="container-fluid">
+		    	<div class="navbar-header">		
+		    		<form class="navbar-form navbar-left" role="search">
+		    			<label for="basic-search">Tìm kiếm </label>
+					 	<div class="input-group">
+					 		<span class="input-group-addon">
+					 			<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+					 		</span>
+					    	<input type="text" id="basic-search" class="form-control" placeholder="Search" size="50">
+					    	<span class="input-group-btn">
+					       		<button class="btn btn-default" type="button">Go!</button>
+					      	</span>
+					 	</div>
+					  <button type="submit" class="btn btn-default">Submit</button>
+					</form>
+		    	</div>
+			</div>
+		</nav>
+	</div>
+	<div class="col-xs-12">
+		<img src="images/dhcntt.jpg" class="img-rounded col-xs-12">
+	</div>
+</div>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Travel</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body class="green">
+sadsdsdsdasdsds
+<div class="container">
+<nav class="navbar navbar-default">
+  	<div class="container-fluid">
+    	<div class="navbar-header">
+    		<form class="navbar-form navbar-left" role="search">
+			 	<div class="form-group">
+			    	<input type="text" class="form-control" placeholder="Search">
+			 	</div>
+			  <button type="submit" class="btn btn-default">Submit</button>
+			</form>
+    	</div>
+	</div>
+</nav>
+</div>
+</body>
+</html>
